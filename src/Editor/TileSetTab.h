741ff111a6c1bc6156d8dab504f5e33e36f008cc
123tris <tristan.a.metz@gmail.com>
2020-05-17 17:02:37 +0200

    Initial setup of tileseteditor

---
+++ src/Editor/TileSetTab.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <QWidget>
+
+#include "TypeDefinitions.h"
+#include "FileTypes/TileSet.h"
+
+namespace TristeonEditor
+{
+	class TileSetTab : public QWidget
+	{
+	public:
+		explicit TileSetTab(QWidget* parent, const Tristeon::String& path);
+	private:
+		Tristeon::TileSet* tileSet;
+	};
+}
