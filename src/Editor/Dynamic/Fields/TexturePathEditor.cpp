9052e87b743f1122ed4c81952f2bffda96599447
Leon Brands (Desktop) <lajbrands@outlook.com>
2021-06-09 23:02:30 +0200

    Fixed texture paths being reset when the file view is closed

--- src/Editor/Dynamic/Fields/TexturePathEditor.cpp
+++ src/Editor/Dynamic/Fields/TexturePathEditor.cpp
@@ -9,23 +9,26 @@
 
 namespace TristeonEditor
 {
-	TexturePathEditor::TexturePathEditor(const nlohmann::json& pValue, const std::function<void(nlohmann::json)>& pCallback): AbstractJsonEditor(pValue, pCallback)
+	TexturePathEditor::TexturePathEditor(const nlohmann::json& pValue, const std::function<void(nlohmann::json)>& pCallback) : AbstractJsonEditor(pValue, pCallback)
 	{
-		std::string path = _value["path"];
-		_button = new QPushButton(path.c_str());
+		const uint32_t guid = _value.value("guid", 0);
+		_button = new QPushButton(QString(std::to_string(guid).c_str()));
 		_widget = _button;
-		QWidget::connect(_button, &QPushButton::clicked, 
+		QWidget::connect(_button, &QPushButton::clicked,
 			[=]()
 			{
 				QDir const baseDir(Tristeon::Settings::assetPath().c_str());
 
 				auto const path = QFileDialog::getOpenFileName(nullptr, QWidget::tr("Find Texture"), Tristeon::Settings::assetPath().c_str(), QWidget::tr("Image Files (*.png *.jpg *.bmp)"));
-				auto const localPath = baseDir.relativeFilePath(path);
-				auto const fileName = QFileInfo(path).baseName();
+				if (!path.isEmpty())
+				{
+					auto const localPath = baseDir.relativeFilePath(path);
+					auto const fileName = QFileInfo(path).baseName();
 
-				_value["guid"] = Tristeon::AssetDatabase::guid("assets://" + localPath.toStdString());
-				_button->setText(fileName);
-				_callback(_value);
+					_value["guid"] = Tristeon::AssetDatabase::guid("assets://" + localPath.toStdString());
+					_button->setText(fileName);
+					_callback(_value);
+				}
 			});
 	}
 
